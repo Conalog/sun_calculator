71493dba2d53abf2351c361d519065b21bb7c4d0
Seojin Felix Kim <sfelixkim@gmail.com>
2023-04-05 16:11:36 +0900

    update examples

--- examples/get_position.py
+++ examples/get_position.py
@@ -1,13 +1,32 @@
 from sun_calculator import get_position, get_times, from_julian, to_julian
-from datetime import datetime, timezone
+from datetime import datetime, timedelta, timezone
 import numpy as np
 
-print(get_position(datetime.now(tz=timezone.utc), lat=37.478897, lng=126.953309))
-for key, val in get_times(datetime.now(tz=timezone.utc), lat=37.478897, lng=126.953309).items():
-    print(key, val.astimezone())
+latitude = 37.478897
+longitude = 126.953309
+
+print("tz-naive local time result:", get_position(date=datetime.now(), lat=latitude, lng=longitude))  # wrong result
+print("tz-aware local time result:", get_position(date=datetime.now().astimezone(), lat=latitude, lng=longitude))
+print("tz-naive UTC", get_position(date=datetime.utcnow(), lat=latitude, lng=longitude))
+print("tz-aware UTC", get_position(date=datetime.now(tz=timezone.utc), lat=latitude, lng=longitude))
+
 
 print(np.array((datetime.now(tz=timezone.utc).replace(tzinfo=None),)*2, dtype=np.datetime64).dtype)
-print(get_position(np.array((datetime.now(tz=timezone.utc).replace(tzinfo=None),)*2, dtype=np.datetime64), lat=np.array((37.478897, 37.478897)), lng=np.array((126.953309, 126.953309))))
+print(get_position(
+    date=np.array([datetime.utcnow()], dtype=np.datetime64),
+    lat=np.array([latitude] * 2),
+    lng=longitude
+))
+
+for key, val in get_times(date=datetime.now(tz=timezone.utc), lat=latitude, lng=longitude).items():
+    print(key, val.astimezone())
+
 print(from_julian(2440588 + 21.))
 print(from_julian(2440588 + np.arange(4)))
 print(to_julian(from_julian(2440588 + np.arange(4))))
+
+print(get_times(
+    date=np.array([datetime.utcnow(), datetime.utcnow() - timedelta(hours=14)], dtype=np.datetime64),
+    lat=np.array([latitude]),
+    lng=np.array([longitude])
+))
--- examples/get_sunrise.py
+++ examples/get_sunrise.py
@@ -2,6 +2,6 @@ from sun_calculator import get_times, DEFAULT_TIMES
 from datetime import datetime, timedelta, timezone
 
 tz_local = timezone(timedelta(hours=9))
-day = datetime(2022, 12, 14, tzinfo=tz_local)
-for time_str, time in get_times(day.astimezone(timezone.utc), lat=37.478897, lng=126.953309, times=DEFAULT_TIMES).items():
+day = datetime.now(tz=tz_local).replace(hour=12, minute=0, second=0, microsecond=0)
+for time_str, time in get_times(day.astimezone(timezone.utc), lat=+37.478897, lng=126.953309, times=DEFAULT_TIMES).items():
     print(time_str, time.astimezone(tz_local).isoformat())
