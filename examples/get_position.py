d90e6eb8409a4b455d7a400995e949220d48b0fb
Chanyub.Park <cypark@conalog.com>
2023-04-21 02:12:01 +0000

    run black

--- examples/get_position.py
+++ examples/get_position.py
@@ -1,32 +1,56 @@
-from sun_calculator import get_position, get_times, from_julian, to_julian
 from datetime import datetime, timedelta, timezone
+
 import numpy as np
+
+from sun_calculator import from_julian, get_position, get_times, to_julian
 
 latitude = 37.478897
 longitude = 126.953309
 
-print("tz-naive local time result:", get_position(date=datetime.now(), lat=latitude, lng=longitude))  # wrong result
-print("tz-aware local time result:", get_position(date=datetime.now().astimezone(), lat=latitude, lng=longitude))
+print(
+    "tz-naive local time result:",
+    get_position(date=datetime.now(), lat=latitude, lng=longitude),
+)  # wrong result
+print(
+    "tz-aware local time result:",
+    get_position(date=datetime.now().astimezone(), lat=latitude, lng=longitude),
+)
 print("tz-naive UTC", get_position(date=datetime.utcnow(), lat=latitude, lng=longitude))
-print("tz-aware UTC", get_position(date=datetime.now(tz=timezone.utc), lat=latitude, lng=longitude))
+print(
+    "tz-aware UTC",
+    get_position(date=datetime.now(tz=timezone.utc), lat=latitude, lng=longitude),
+)
 
 
-print(np.array((datetime.now(tz=timezone.utc).replace(tzinfo=None),)*2, dtype=np.datetime64).dtype)
-print(get_position(
-    date=np.array([datetime.utcnow()], dtype=np.datetime64),
-    lat=np.array([latitude] * 2),
-    lng=longitude
-))
+print(
+    np.array(
+        (datetime.now(tz=timezone.utc).replace(tzinfo=None),) * 2, dtype=np.datetime64
+    ).dtype
+)
+print(
+    get_position(
+        date=np.array([datetime.utcnow()], dtype=np.datetime64),
+        lat=np.array([latitude] * 2),
+        lng=longitude,
+    )
+)
 
-for key, val in get_times(date=datetime.now(tz=timezone.utc), lat=latitude, lng=longitude).items():
+for key, val in get_times(
+    date=datetime.now(tz=timezone.utc), lat=latitude, lng=longitude
+).items():
     print(key, val.astimezone())
 
-print(from_julian(2440588 + 21.))
+print(from_julian(2440588 + 21.0))
 print(from_julian(2440588 + np.arange(4)))
 print(to_julian(from_julian(2440588 + np.arange(4))))
 
-print(get_times(
-    date=np.array([datetime.utcnow(), datetime.utcnow() - timedelta(hours=14)], dtype=np.datetime64),
-    lat=np.array([latitude]),
-    lng=np.array([longitude])
-))
+print(
+    get_times(
+        date=np.array(
+            [datetime.utcnow(), datetime.utcnow() - timedelta(hours=14)],
+            dtype=np.datetime64,
+        ),
+        lat=np.array([latitude]),
+        lng=np.array([longitude]),
+    )
+)
--- examples/get_sunrise.py
+++ examples/get_sunrise.py
@@ -1,7 +1,10 @@
-from sun_calculator import get_times, DEFAULT_TIMES
 from datetime import datetime, timedelta, timezone
+
+from sun_calculator import DEFAULT_TIMES, get_times
 
 tz_local = timezone(timedelta(hours=9))
 day = datetime.now(tz=tz_local).replace(hour=12, minute=0, second=0, microsecond=0)
-for time_str, time in get_times(day.astimezone(timezone.utc), lat=+37.478897, lng=126.953309, times=DEFAULT_TIMES).items():
+for time_str, time in get_times(
+    day.astimezone(timezone.utc), lat=+37.478897, lng=126.953309, times=DEFAULT_TIMES
+).items():
     print(time_str, time.astimezone(tz_local).isoformat())
